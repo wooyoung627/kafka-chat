3f51cd019296fef9d052c24e4e6e435edba3a1a9
wylee <dldndud627@gmail.com>
2022-07-25 17:43:08 +0900

    메시지 시간 추가

--- src/main/java/com/kafka/chat/dto/Message.java
+++ src/main/java/com/kafka/chat/dto/Message.java
@@ -2,15 +2,22 @@ package com.kafka.chat.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class Message {
     private User user;
     private String msg;
 
+    private LocalDateTime time = LocalDateTime.now();
+
     public String toJson(){
         StringBuilder json = new StringBuilder();
         json.append("{\"user\" : " + this.user.toJson() + " , ");
-        json.append("\"msg\" : \"" + this.msg + "\" }");
+        json.append("\"msg\" : \"" + this.msg + "\" ," +
+                "\"time\" : \"" + this.time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+ "\" }");
         return json.toString();
     }
 }
